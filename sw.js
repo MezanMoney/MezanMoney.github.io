3e6ea1a65ed5cef8d8b4b14f5c5cd2d4e1cc6e8a
MezanMoney <fs.almutairi@outlook.com>
2026-09-09 20:55:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='mizan-secure-v5-7-0';
-const SHELL=['./','./index.html','./secure-v3.html?v=570','./manifest.webmanifest'];
+const CACHE='mizan-secure-v6-0-0';
+const SHELL=['./','./index.html','./secure-v3.html?v=600','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
   const r=e.request;if(r.method!=='GET')return;
-  e.respondWith(fetch(r).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(r,c).catch(()=>{}));return res}).catch(()=>caches.match(r).then(m=>m||caches.match('./secure-v3.html?v=570'))))
+  e.respondWith(fetch(r).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(r,c).catch(()=>{}));return res}).catch(()=>caches.match(r).then(m=>m||caches.match('./secure-v3.html?v=600'))))
 });
